a8671e0557957315e1f29e0868c31fb8d669e41d
Laurent <laurentsingamalom@gmail.com>
2025-08-15 17:20:21 +0200

    upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import './index.css'   // <-- doit être la toute première importation CSS
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import './index.css'; // <- doit exister pour que la CSS sorte en prod
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
